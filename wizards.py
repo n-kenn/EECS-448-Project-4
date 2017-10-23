91e2277a7db80741ef06d016ba27457911943c4d
Josh Oertel <j583o901@ku.edu>
2017-10-23 14:54:16 -0500

    made a little ground thing, nothing special

--- wizards.py
+++ wizards.py
@@ -1,9 +1,13 @@
 import sys
 import pygame as pg
+from ground import Ground
 
 width, height = 1024, 512
 FPS = 30
-black = (0, 0, 0)
+colors = {
+    'black': (0, 0, 0),
+    'white': (255, 255, 255)
+}
 
 pg.init()
 display = pg.display.set_mode((width, height))
@@ -11,7 +15,7 @@ pg.display.set_caption('Wizards')
 clock = pg.time.Clock()
 
 
-def quitCheck():
+def quit_check():
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
@@ -19,7 +23,9 @@ def quitCheck():
 
 
 while True:
-    quitCheck()
-    display.fill(black)
+    quit_check()
+    display.fill(colors['black'])
+    ground = Ground((width, height / 2), colors['white'])
+    display.blit(ground, (0, height / 2))
     pg.display.update()
     clock.tick(FPS)
