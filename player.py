5caeb8fa7404f483b5cb378b2bb92320a7e1e377
joshwashywash <joshwashywash@gmail.com>
2017-10-25 23:44:21 -0500

    small changes in syntax and remove a redudant import in player.py

--- player.py
+++ player.py
@@ -1,12 +1,12 @@
-from pygame import sprite, Surface, Rect
+from pygame import sprite, Surface
 
 
 class Player(sprite.Sprite):
 
-    def __init__(self, size, start, color):
+    def __init__(self, size, start_pos, color):
         super(Player, self).__init__()
         self.image = Surface(size)
-        self.rect = self.image.get_rect(bottom=start)
+        self.rect = self.image.get_rect(bottom=start_pos)
         self.image.fill(color)
 
     def move(self, x, y):
