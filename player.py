31a664c6a70dda2a0b56a52210584f20d5facd46
joshwashywash <joshwashywash@gmail.com>
2017-11-03 18:12:28 -0500

    player can now jump. may need some tweaking on values, also os should be able to load all images.

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from pygame import mask, math
 from pygame.locals import *
 from animated_sprite import Animated_Sprite
 
+
 class Player(Animated_Sprite):
 
     def __init__(self, file_name, size, frame_rate, start_pos):
@@ -10,6 +11,7 @@ class Player(Animated_Sprite):
         self.rect = self.image.get_rect(bottomleft=start_pos)
         self.vel = math.Vector2(0, 0)
         self.direction = None
+        self.landed = True
 
     def keep_in_bounds(self, bounds):
         if self.rect.right > bounds.right:
@@ -17,18 +19,21 @@ class Player(Animated_Sprite):
         elif self.rect.left < bounds.left:
             self.rect.left = bounds.left
 
-    def jump(self):
-        pass
-
     def check_keys(self, keys):
+        self.vel = math.Vector2(0, 0)
         if keys[K_LEFT]:
-            self.vel.x -= 5
+            self.vel.x -= 10
         elif keys[K_RIGHT]:
-            self.vel.x += 5
+            self.vel.x += 10
+        if keys[K_SPACE] and self.landed:
+            self.landed = False
+            self.vel.y -= 40
 
     def update(self, world):
+        self.vel.y += world['gravity']
         super(Player, self).update()
         self.rect.move_ip(self.vel)
         self.keep_in_bounds(world['ground'].rect)
-        # if self.rect.colliderect(world['ground'].rect):
-        #     self.rect.bottom = world['ground'].rect.top
+        if self.rect.colliderect(world['ground'].rect):
+            self.landed = True
+            self.rect.bottom = world['ground'].rect.top
--- wizards.py
+++ wizards.py
@@ -13,7 +13,7 @@ display = pg.display.set_mode((width, height))
 clock = pg.time.Clock()
 
 ground = Ground(os.path.join('world', 'ground.png'), (display.get_rect().left,
-                                     height / 2), pg.Color('white'))
+                                                      height / 2), pg.Color('white'))
 
 player = Player(os.path.join('sprite_sheets', 'spiral.png'),
                 (0, 0, 32, 32), 10, ground.rect.topleft)
@@ -26,6 +26,7 @@ world = {
     'ground': ground
 }
 
+
 def quit_check():
     for event in pg.event.get():
         if event.type is pg.QUIT:
@@ -34,16 +35,19 @@ def quit_check():
         elif event.type is pg.KEYDOWN:
             player.check_keys(pg.key.get_pressed())
         elif event.type is pg.KEYUP:
-            player.vel.x = player.vel.y = 0
+            if event.key == pg.K_LEFT or event.key == pg.K_RIGHT:
+                player.vel.x = 0
             # fallables.add(Explosive((32, 32), display.get_rect().midtop, pg.Color('green'), [ground, player]))
 
-while True:
-    quit_check()
-    display.blit(pg.image.load(os.path.join('world', 'sky.png')), (0, 0))
-    fallables.update(world)
-    statics.update()
-    fallables.draw(display)
-    statics.draw(display)
-    pg.display.update()
-    pg.display.set_caption('Wizards {:.2f}'.format(clock.get_fps()))
-    clock.tick(FPS)
+
+if __name__ == '__main__':
+    while True:
+        quit_check()
+        display.blit(pg.image.load(os.path.join('world', 'sky.png')), (0, 0))
+        fallables.update(world)
+        statics.update()
+        fallables.draw(display)
+        statics.draw(display)
+        pg.display.update()
+        pg.display.set_caption('Wizards {:.2f}'.format(clock.get_fps()))
+        clock.tick(FPS)
