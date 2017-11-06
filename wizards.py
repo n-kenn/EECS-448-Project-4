178a4a3dc44a4ab6b77d888c28a88357090751af
Unknown <nkenn@ku.edu>
2017-11-05 20:55:24 -0600

    Correct player fires the shot

--- wizards.py
+++ wizards.py
@@ -39,9 +39,9 @@ def check_keys():
             pg.quit()
             sys.exit()
         elif event.type is pg.MOUSEBUTTONDOWN:
-            active_player = turn_handler.switch_turns()
             active_player.fire(pg.mouse.get_pos(), [
                                world.ground, turn_handler.inactive_player])
+            active_player = turn_handler.switch_turns()
 
 
 if __name__ == '__main__':
