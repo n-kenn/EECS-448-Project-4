ed7c7326debf4dd6a23b06aacc4fe272bf81fcf3
Unknown <nkenn@ku.edu>
2017-11-05 21:24:10 -0600

    Now have checking for game over

--- wizards.py
+++ wizards.py
@@ -14,7 +14,7 @@ FPS = 60
 pg.init()
 display = pg.display.set_mode((width, height))
 clock = pg.time.Clock()
-
+game_over = False
 world = World(pg.image.load(os.path.join('images', 'sky.png')).convert(), Ground(pg.image.load(
     os.path.join('images', 'ground.png')).convert_alpha(), (display.get_rect().left, height / 2)), 0.1)
 
@@ -22,7 +22,7 @@ players = [
     Player(pg.image.load(os.path.join('sprite_sheets', 'wizard.png')).convert_alpha(),
            (0, 0, 32, 32), 5, world.ground.rect.topleft),
     Player(pg.image.load(os.path.join('sprite_sheets', 'wizard.png')).convert_alpha(),
-           (0, 0, 32, 32), 5, world.ground.rect.midtop)
+           (0, 0, 32, 32), 5, world.ground.rect.topright)
 ]
 
 turn_handler = Turn_Handler(players)
@@ -43,14 +43,26 @@ def check_keys():
                                world.ground, turn_handler.inactive_player])
             active_player = turn_handler.switch_turns()
 
+def check_players():
+    for i in range(0,2):
+        if (not players[i].alive()):
+            fon = pg.font.SysFont("comicsansms",40)
+            victor = "Player 1 wins" if i == 1 else "Player 2 wins"
+            display.blit(fon.render(victor, 0, pg.Color('black')),(10,10))
+            return True
+    return False
+
 
 if __name__ == '__main__':
     while True:
-        check_keys()
+        if not game_over:
+            check_keys()
         fallables.update(world)
         statics.update()
         statics.draw(display)
         fallables.draw(display)
+        if not game_over:
+            game_over = check_players()
         pg.display.update()
         pg.display.set_caption('Wizards {:.2f}'.format(clock.get_fps()))
         clock.tick(FPS)
